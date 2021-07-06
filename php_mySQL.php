414a606bb6dadc6206b7f84ae8474bef6c1bb93d
peppe-p <ombrachiara96@gmail.com>
2021-07-06 15:02:16 +0200

    create request for connect to DB

--- php_mySQL.php
+++ php_mySQL.php
@@ -5,3 +5,11 @@ define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PW", "root");
 define("DB_NAME", "db_university");
+
+// Connessione al database
+$request = new mysqli(DB_HOST, DB_USERNAME, DB_PW, DB_NAME);
+if ($request && $request->connect_error) {
+    echo "Connessione fallita:" . $request->connect_error;
+} else {
+    echo "Richiesta avvenuta con successo!";
+}
